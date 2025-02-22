8fb1a04888e1f789f2468ed09d8496d0d0c30a63
Andrew Yang <andrewyang200@gmail.com>
2025-02-22 00:35:51 -0500

    welcome screen

--- sharify/screens/auth/WelcomeScreen.jsx
+++ sharify/screens/auth/WelcomeScreen.jsx
@@ -8,7 +8,7 @@ const WelcomeScreen = ({ navigation }) => {
 
     return (
         <ImageBackground
-            source={require('../../assets/welcomescreen.jpg')} // Add your image in the assets folder
+            source={require('../../assets/image.jpg')} // Add your image in the assets folder
             style={styles.backgroundImage}
             resizeMode="cover"
         >
